52e8a7fbb8cb7f5d8cddc925123005c50383fb4b
Arkadiy <arkadiy.samokhvalov@bk.ru>
2020-01-31 14:44:12 +0300

    Добавил усложненное 6

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 "use strict";
 
-function guessNumber () {
-  let number = 37;
+function guessNumber() {
+  let number = randomInteger(),
+      attempts = 10;
 
-  function checkVersion () {
-    let version = +prompt('Угадай число от 1 до 100!');
-    console.log('version: ', version);
-    if (version !== 0) {
-      if (version === number) {
-        alert('Ты угадал!')
+  function checkVersion() {
+    let version = prompt('Угадайте число от 1 до 100!');
+
+    if (parseInt(version)) {
+      if (version == number) {
+        if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) return guessNumber();
       } else if (version > number) {
-        alert('Загаданное число меньше!');
-        return guessNumber();
+        alert('Загаданное число меньше, осталось попыток ' + --attempts);
+        if (attempts === 0)
+          if (confirm('Попытки закончились, хотели бы сыграть еще?')) return guessNumber();
+          else {
+            alert('Спасибо за игру! Приходите ещё!');
+            return null;
+          }
+        return checkVersion();
       } else {
-        alert('Загаданное число больше!');
-        return guessNumber();
+        alert('Загаданное число больше, осталось попыток ' + --attempts);
+        if (attempts === 0)
+          if (confirm('Попытки закончились, хотели бы сыграть еще?')) return guessNumber();
+          else {
+            alert('Спасибо за игру! Приходите ещё!');
+            return null;
+          }
+        return checkVersion();
+      }
+    } else {
+      if(version === null) alert('Спасибо за игру! Приходите ещё!');
+      else {
+        alert('Введи число!'); 
+        return checkVersion();
       }
     }
   }
 
   return checkVersion();
+}
+
+function randomInteger() {
+  let rand = 1 + Math.random() * (99 + 1 - 1);
+  return Math.floor(rand);
 }
 
 guessNumber();
